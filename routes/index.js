3f44dd0e82fd85c939c97e35f71cf930858ac0a6
Bharath Kumar Narahari <naraharibharathkumar@gmail.com>
2016-12-08 17:43:30 -0800

    Remove the question mark in the name of the admin

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.post('/sendEmail', function (req, res, next) {
     var transporter = nodemailer.createTransport('smtps://archerlml277@gmail.com:081152016@smtp.gmail.com');
     if(type=='Notification'){
         var mailOptions = {
-            from: '"Archer Lucy ?" <archerlml277@gmail.com>', // sender address
+            from: '"Archer Lucy" <archerlml277@gmail.com>', // sender address
             to: emailId+', '+emailId, // list of receivers
             subject: 'Report Status Changed', // Subject line
             text: 'The Status of the report you submitted has been changed, Please login to the app to check the updates', // plaintext body
@@ -34,7 +34,7 @@ router.post('/sendEmail', function (req, res, next) {
     }
     else if(type=='Confirmation'){
         var mailOptions = {
-            from: '"Archer Lucy ?" <archerlml277@gmail.com>', // sender address
+            from: '"Archer Lucy" <archerlml277@gmail.com>', // sender address
             to: emailId+', '+emailId, // list of receivers
             subject: 'Report Resolved', // Subject line
             text: 'The Report you submitted has been resolved', // plaintext body
